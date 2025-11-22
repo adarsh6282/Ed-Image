e35306abb35102291eb005ec5972dbd80f22023a
Adarsh KP <kpadarsh41@gmail.com>
2025-11-22 12:59:41 +0530

    added resend api for otp

--- server/src/utils/sendMail.ts
+++ server/src/utils/sendMail.ts
@@ -1,31 +1,31 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_KEY);
 
 export const sendMail = async (email: string, otp: string) => {
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: `Verify your Account`,
-    html: `<p>We received a request to verify your account. Your One-Time Password (OTP) is:</p>
-                <div style="text-align: center; margin: 30px 0;">
-                    <div style="display: inline-block; background-color: #edf2f7; border: 1px dashed #cbd5e0; border-radius: 6px; padding: 15px 30px;">
-                        <span style="font-family: 'Courier New', monospace; font-size: 28px; font-weight: bold; color: #3182ce; letter-spacing: 5px;">${otp}</span>
-                    </div>
-                </div>
-                <p style="text-align: center;">This code will expire in 5 minutes.</p>`,
-  };
+  try {
+    await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: email,
+      subject: "Verify your Account",
+      html: `
+        <p>We received a request to verify your account. Your One-Time Password (OTP) is:</p>
+        <div style="text-align: center; margin: 30px 0;">
+          <div style="display: inline-block; background-color: #edf2f7; border: 1px dashed #cbd5e0; border-radius: 6px; padding: 15px 30px;">
+            <span style="font-family: 'Courier New', monospace; font-size: 28px; font-weight: bold; color: #3182ce; letter-spacing: 5px;">
+              ${otp}
+            </span>
+          </div>
+        </div>
+        <p style="text-align: center;">This code will expire in 5 minutes.</p>
+      `,
+    });
 
-  await transporter.sendMail(mailOptions);
+    console.log("OTP email sent!");
+  } catch (error) {
+    console.error("Failed to send email:", error);
+  }
 };
